05a691c2aca2501f32410b72553f189019a3bbbf
enricodifranco <enricodifranco96@gmail.com>
2025-06-10 22:05:48 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const rootElement = document.getElementById("root");
+ReactDOM.createRoot(rootElement).render(<App />);
