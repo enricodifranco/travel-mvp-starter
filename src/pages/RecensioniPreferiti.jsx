4b15e6a20f7559fc6a201725a3fe3e2e08849d51
enricodifranco <enricodifranco96@gmail.com>
2025-06-11 10:39:32 +0200

    Update and rename Recensioni.js to RecensioniPreferiti.jsx

---
+++ src/pages/RecensioniPreferiti.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RecensioniPreferiti = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Recensioni e Preferiti</h1>
+      <p>Qui troverai le recensioni degli utenti e i tuoi itinerari salvati.</p>
+    </div>
+  );
+};
+
+export default RecensioniPreferiti;
