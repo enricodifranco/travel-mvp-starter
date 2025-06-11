4bcc505669939a800ad90549a22745a581f91982
enricodifranco <enricodifranco96@gmail.com>
2025-06-11 09:25:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ import Proposte from './pages/Proposte';
 import Itinerario from './pages/Itinerario';
 import Contatti from './pages/Contatti';
 import Recensioni from './pages/Recensioni';
+import Navbar from './components/Navbar';
+
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/proposte" element={<Proposte />} />
-        <Route path="/itinerario" element={<Itinerario />} />
-        <Route path="/contatti" element={<Contatti />} />
-        <Route path="/recensioni" element={<Recensioni />} />
-      </Routes>
+      <div className="bg-gray-50 min-h-screen font-sans">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/proposte" element={<Proposte />} />
+          <Route path="/itinerario" element={<Itinerario />} />
+          <Route path="/contatti" element={<Contatti />} />
+          <Route path="/recensioni" element={<Recensioni />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
