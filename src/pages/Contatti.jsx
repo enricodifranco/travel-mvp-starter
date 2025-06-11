4c5817ef62a1609e51ee9d4d932508d11b0044d0
enricodifranco <enricodifranco96@gmail.com>
2025-06-11 10:39:15 +0200

    Update and rename Contatti.js to Contatti.jsx

---
+++ src/pages/Contatti.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Contatti = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Contattaci</h1>
+      <p>Email: supporto@travelmvp.com</p>
+    </div>
+  );
+};
