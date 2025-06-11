94c06043157c44f6f319133ffeeb89f22baba510
enricodifranco <enricodifranco96@gmail.com>
2025-06-11 10:03:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage';
-import ProposteItinerario from './pages/Proposte';
-import ItinerarioScelto from './pages/Itinerario';
+import Proposte from './pages/Proposte';
+import Itinerario from './pages/Itinerario';
 import Contatti from './pages/Contatti';
-import RecensioniPreferiti from './pages/Recensioni';
+import Recensioni from './pages/Recensioni';
 import Navbar from './components/Navbar';
 import './App.css';
 
